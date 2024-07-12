1c373a74a5ec037767ff671a80c034fd1caf5562
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-07-12 16:55:06 +0600

    complete

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
       origin: [
-            'http://localhost:5173',
+            'https://a11-dev-connect-bd.web.app',
       ],
       credentials: true
 }));
